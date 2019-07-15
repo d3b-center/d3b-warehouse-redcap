1f935488757ef69a40a088dc6a0285a3ef985942
Avi Kelman <patcherton.fixesthings@gmail.com>
2019-07-15 12:03:07 -0400

    :loud_sound: Report BRP request errors

--- brp.py
+++ brp.py
@@ -1,6 +1,17 @@
+import requests
 from session import requests_retry_session
+import pprint
 
 BRP_API = 'https://brp.research.chop.edu/api/'
+
+
+class RequestException(Exception):
+    def __init__(self, msg, req, resp):
+        super().__init__(
+            '\n\n' + msg + '\n' + pprint.pformat({
+                "sent": vars(req), "received": vars(resp)
+            })
+        )
 
 
 class BRP:
@@ -10,15 +21,19 @@ class BRP:
     def request(self, type, endpoint, json=None):
         while endpoint[0] == '/':
             endpoint = endpoint[1:]
-        return requests_retry_session().request(
+        req = requests.Request(
             type,
             BRP_API + endpoint,
             json=json,
             headers={
                 'Content-Type': 'application/json',
-                'Authorization': 'token ' + self.api_token
+                'Authorization': 'token ' + str(self.api_token)
             }
-        ).json()
+        ).prepare()
+        resp = requests_retry_session().send(req)
+        if resp.status_code not in {200, 201}:
+            raise RequestException('Status not 200/201', req, resp)
+        return resp.json()
 
     def get_subjects(self, protocol):
         resp = self.request('get', 'protocols/' + str(protocol) + '/subjects/')

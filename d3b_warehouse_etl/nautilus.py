d0ddfe23c8066d4ec5258c8f7b1675a5f50cdf58
Avi Kelman <patcherton.fixesthings@gmail.com>
2019-08-28 15:55:28 -0400

    :recycle: Streamline db config and query code

--- d3b_warehouse_etl/nautilus.py
+++ d3b_warehouse_etl/nautilus.py
@@ -1,32 +1,15 @@
 #!/usr/bin/python
-import psycopg2
 import pandas as pd
+import psycopg2
+from config import nautilus_config
 
 
-def get_nautilus_data(params, nautilus_irb):
-    """ Connect to the Nautilus PostgreSQL datawarehouse database """
-    conn = None
-    try:
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
-
-        # create a cursor
-        cur = conn.cursor()
-
-        cur.execute(
+def get_nautilus_data(nautilus_irb):
+    """
+    Get aliquot data from the Nautilus PostgreSQL database for the given study
+    """
+    with psycopg2.connect(**nautilus_config) as conn:
+        return pd.read_sql(
             "SELECT * from eig_nautilus_aliquot "
-            "where study_name='{}'".format(nautilus_irb))
-
-        names = [x[0] for x in cur.description]
-        sample_information = pd.DataFrame(cur.fetchall(), columns=names)
-
-        # close the communication with the PostgreSQL
-        cur.close()
-        return sample_information
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
+            f"where study_name='{nautilus_irb}'"
+        )
